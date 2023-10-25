371367d5b6496ce1dd26d850566fe5d30cf3b034
Simøn <simonsaillen29@gmail.com>
2023-10-24 21:09:31 -0300

    Add files via upload

---
+++ TP4.ino
@@ -0,0 +1,74 @@
+#include <SPI.h>
+#include <RF24.h>
+#include <nRF24L01.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#define PinHUM A0
+#define PIN_TEMP D2
+#define PIN_CE D9
+#define PIN_CS D10
+
+// Objetos
+RF24 radio(PIN_CE, PIN_CS);
+DHT dht(PIN_TEMP, DHT11);
+
+// Definicion de datos
+struct Data {
+  int Modulo_id;
+  float Temperatura;
+  float Humedad;
+};
+
+unsigned long long Adress = 0xB3B4B5B6CDLL;
+unsigned long Tiempo;
+
+// Retorna la Temperatura
+float getTemperatura() {
+  return dht.readTemperature();
+}
+
+// Retorna la Humedad
+float getHumedad() {
+  return float(analogRead(PinHUM));
+}
+
+// Imprime los datos
+void ImprimirMensaje(bool aux, Data datos){
+  if(aux) {
+      Serial.println("Datos Enviados con Exito: ");
+      Serial.println("Temperatura: " + String(datos.Temperatura));
+      Serial.println("Humedad: " + String(datos.Humedad));
+    } else {
+      Serial.println("Error al mandar los datos");
+      Serial.println("Temperatura: " + String(datos.Temperatura));
+      Serial.println("Humedad: " + String(datos.Humedad));
+    }
+}
+
+// ----------------------
+
+// Configuraciones
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  dht.begin();
+
+  radio.openWritingPipe(Adress);
+  radio.setDataRate(RF24_250KBPS);
+  Tiempo = millis();
+}
+
+// Programa Principal
+void loop() {    // Se realiza cada 1 hora
+  if(millis() >= Tiempo) {  // Analiza la bandera
+    Data mediciones;
+    mediciones.Modulo_id = 2;
+    mediciones.Temperatura = getTemperatura();
+    mediciones.Humedad = getHumedad();
+
+    bool OK = radio.write(&mediciones, sizeof(mediciones));
+    ImprimirMensaje(OK,mediciones);
+    Tiempo += 3000;    // Aumento la bandera 1 hora
+  }
+}
