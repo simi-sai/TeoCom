ae47d6aebe3dbed656d2c458ab56d26c49ea8eaa
Simøn <simonsaillen29@gmail.com>
2023-11-03 17:36:20 -0300

    Version Ultima
    3/11

--- TP4.ino
+++ TP4.ino
@@ -5,11 +5,10 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
-#define Pin_HUM A0
+#define PIN_HUM A0
 #define PIN_TEMP D2
 #define PIN_CE D9
 #define PIN_CS D10
-#define Boton D3
 
 // Objetos
 RF24 radio(PIN_CE, PIN_CS);
@@ -26,9 +25,7 @@ struct Data {
 // Declaracion de variables
 uint64_t Adress = 0xB3B4B5B6CDLL;
 unsigned long Tiempo;
-Data recoleccion[24][3];
-int Indice_1;
-int Indice_2;
+Data mediciones;
 
 // Retorna la Temperatura
 float getTemperatura() {
@@ -37,16 +34,11 @@ float getTemperatura() {
 
 // Retorna la Humedad
 float getHumedad() {
-  return map(analogRead(Pin_HUM), 1024, 300, 0, 100);
+  return map(analogRead(PIN_HUM), 1024, 300, 0, 100);
 }
 
 // Imprime los datos
 void ImprimirMensaje(bool aux, Data datos){
-  if(aux) {
-    Serial.println("Datos Enviados con Exito: ");
-  } else {
-    Serial.println("Error al mandar los datos: ");
-  }
   Serial.println("Temperatura: " + String(datos.Temperatura));
   Serial.println("Humedad: " + String(datos.Humedad));
 }
@@ -62,29 +54,10 @@ void imprimirLCD(Data mediciones){
   lcd.print("%");
 }
 
-// Guarda los Datos
-void GuardarDatos(Data datos) {
-  recoleccion[Indice_1][Indice_2] = datos;
-  Indice_1++;
-  // Para evitar el stack overflow
-  if(Indice_1 == 24) {
-    Indice_1 = 0;
-    Indice_2 ++;
-    EnviarSerial();
-    if(Indice_2 == 3) {
-      Indice_2 = 0;
-    }
-  }
-}
-
-// Envia por serial la matriz guardada
-void EnviarSerial() {
-  for(int i = 0; i < 24; i++) {
-    Serial.println(String(recoleccion[i][0].Temperatura) + "   " + String(recoleccion[i][1].Temperatura) + "   " + String(recoleccion[i][2].Temperatura));
-    Serial.println(String(recoleccion[i][0].Humedad) + "   " + String(recoleccion[i][1].Humedad) + "   " + String(recoleccion[i][2].Humedad));
-    Serial.println("----------");
-  }
-  Serial.println("- - - - - - - - - - -");
+void Medir() {
+  mediciones.Modulo_id = 2;
+  mediciones.Temperatura = getTemperatura();
+  mediciones.Humedad = getHumedad();
 }
 
 // ----------------------
@@ -95,36 +68,32 @@ void setup() {
   radio.begin();
   dht.begin();
 
-  //pinMode(Boton, INPUT);
-  //attachInterrupt(digitalPinToInterrupt(Boton), EnviarSerial, CHANGE);
-
   radio.setPALevel(RF24_PA_LOW);
   radio.openWritingPipe(Adress);
   radio.setDataRate(RF24_250KBPS);
   radio.setChannel(100);
 
   Tiempo = millis();
-  Indice_1 = 0;
-  Indice_2 = 0;
 
-  //Imprimir LCD
-  //lcd.init();
-  //lcd.backlight();
-  //lcd.clear();
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
 }
 
 // Programa Principal
-void loop() {                   // Se realiza cada 1 hora
+void loop() {           
   if(millis() >= Tiempo) {      // Analiza la bandera
-    Data mediciones; 
-    mediciones.Modulo_id = 2;
-    mediciones.Temperatura = getTemperatura();
-    mediciones.Humedad = getHumedad();
+    Medir();
+    int i = 0;
+    bool OK = false;
 
-    bool OK = radio.write(&mediciones, sizeof(mediciones));
+    do {
+      OK = radio.write(&mediciones, sizeof(mediciones));
+      i++;
+    } while(!OK && i < 5);
+
     ImprimirMensaje(OK, mediciones);
-    GuardarDatos(mediciones);
-    //imprimirLCD(mediciones);
+    imprimirLCD(mediciones);
     Tiempo += 3600000;          // Aumento la bandera 1 hora
   }
 }
